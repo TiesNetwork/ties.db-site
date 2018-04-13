9659ac9979a5177a2c49eafbfcf15d179303fb47
Ivan Vyatkin <vanya095@gmail.com>
2018-04-13 09:32:10 +0300

    Add partners block.

--- src/views/Main/Desktop.js
+++ src/views/Main/Desktop.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Cases from './components/Cases';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
+import Partners from './components/Partners';
 import Roadmap from './components/Roadmap';
 import WhyNeed from './components/WhyNeed';
 
@@ -15,6 +16,7 @@ const LandingMain = () => (
     <WhyNeed />
     <HowItWorks />
     <Cases />
+    <Partners />
     <Roadmap />
   </div>
 )
--- src/views/Main/ducks/reducer.js
+++ src/views/Main/ducks/reducer.js
@@ -22,6 +22,68 @@ const initialState = {
       title: 'FILE SHARING',
     },
   ],
+  partners: [
+    {
+      link: 'https://diwtoken.com/',
+      src: require('../assets/partners/diw.png'),
+      title: 'DIW',
+    },
+    {
+      link: 'https://exenium.io/',
+      src: require('../assets/partners/exenium.png'),
+      title: 'Exenium',
+    },
+    {
+      link: 'https://www.varcrypt.io/',
+      src: require('../assets/partners/varcrypt.png'),
+      title: 'varcrypt',
+    },
+    {
+      link: 'https://credits.com/ru/Home/New_Ins/2257',
+      src: require('../assets/partners/credits.png'),
+      title: 'Credits',
+    },
+    {
+      link: 'https://canya.io/index.html',
+      src: require('../assets/partners/canya.png'),
+      title: 'CanYa',
+    },
+    {
+      link: 'http://www.krtoken.com/',
+      src: require('../assets/partners/krtoken.png'),
+      title: 'KrToken',
+    },
+    {
+      link: 'https://blocklancer.net/',
+      src: require('../assets/partners/blocklancer.png'),
+      title: 'blocklancer',
+    },
+    {
+      link: 'https://wishknish.com/',
+      src: require('../assets/partners/wishknish.png'),
+      title: 'WishKnish',
+    },
+    {
+      link: 'http://www.ecos.ee/',
+      src: require('../assets/partners/ecos.png'),
+      title: 'ECOS',
+    },
+    {
+      link: 'https://medium.com/iagon-official/iagon-and-ties-db-partnership-410ffb1a4192',
+      src: require('../assets/partners/iagon.png'),
+      title: 'IAGON',
+    },
+    {
+      link: 'https://bcharity.io/',
+      src: require('../assets/partners/bcharity.png'),
+      title: 'BCharity',
+    },
+    {
+      link: 'https://hoqu.io',
+      src: require('../assets/partners/hoqu.png'),
+      title: 'HOQU',
+    },
+  ],
   roadmap: [
     {
       description: 'API prototyping. White paper published.',
