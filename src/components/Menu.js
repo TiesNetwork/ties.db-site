be7f4fef834a1f1b797bb2ae54daa00cdfb1db97
Ivan Vyatkin <vanya095@gmail.com>
2018-04-17 00:20:42 +0300

    Mobile menu.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,66 +1,80 @@
 import classNames from 'classnames';
 import React, { createElement } from 'react';
+import { compose, withStateHandlers } from 'recompose';
 
 import styles from './Menu.scss';
 
 const Menu = ({
   menu,
-}) => (
-  <div className={styles.Root}>
-    <div className={styles.Company}>
-      <img
-        alt="Ties.DB"
-        className={styles.CompanyLogo}
-        src={require('../views/Main/assets/logo.svg')}
-      />
+  onHamburgerClick,
+  opened,
+}) => {
+  const className = classNames(styles.Root, {
+    [styles.RootOpened]: !!opened
+  });
 
-      <div className={styles.CompanyRate}>
-        <strong>1 TIE</strong> = 0.671125$ (+12,24%)
+  return (
+    <div className={className}>
+      <div className={styles.Company}>
+        <img
+          alt="Ties.DB"
+          className={styles.CompanyLogo}
+          src={require('../views/Main/assets/logo.svg')}
+        />
+
+        <div className={styles.CompanyRate}>
+          <strong>1 TIE</strong> = 0.671125$ (+12,24%)
+        </div>
+
+        <button
+          className={styles.Hamburger}
+          onClick={onHamburgerClick}
+        />
+      </div>
+
+      <div className={styles.Nav}>
+        {menu.map(({ items, link, title }, index) => {
+          const titleClassName = classNames(styles.ItemTitle, {
+            [styles.ItemTitleWithCaret]: !!items,
+            [styles.ItemTitleWithLink]: !!link,
+          });
+
+          return (
+            <div className={styles.Item} key={index}>
+              {createElement(link ? 'a' : 'div', {
+                children: title,
+                className: titleClassName,
+                href: link,
+              })}
+
+              {items && (
+                <div className={styles.ItemSubmenu}>
+                  {items.map(({ link, title }, index) => (
+                    <a
+                      className={styles.ItemLink}
+                      href={link}
+                      key={index}
+                    >
+                      {title}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+
+      <div className={styles.Actions}>
+        <a
+          className={styles.Action}
+          href="/"
+        >
+          Rent your drive
+        </a>
       </div>
     </div>
-
-    <div className={styles.Nav}>
-      {menu.map(({ items, link, title }, index) => {
-        const titleClassName = classNames(styles.ItemTitle, {
-          [styles.ItemTitleWithCaret]: !!items,
-          [styles.ItemTitleWithLink]: !!link,
-        });
-
-        return (
-          <div className={styles.Item} key={index}>
-            {createElement(link ? 'a' : 'div', {
-              children: title,
-              className: titleClassName,
-              href: link,
-            })}
-
-            {items && (
-              <div className={styles.ItemSubmenu}>
-                {items.map(({ link, title }, index) => (
-                  <a
-                    className={styles.ItemLink}
-                    href={link}
-                    key={index}
-                  >
-                    {title}
-                  </a>
-                ))}
-              </div>
-            )}
-          </div>
-        );
-      })}
-    </div>
-
-    <div className={styles.Actions}>
-      <a
-        className={styles.Action}
-        href="/"
-      >
-        Rent your drive
-      </a>
-    </div>
-  </div>
-)
+  );
+}
 
 export default Menu;
--- src/views/Main/components/Header.js
+++ src/views/Main/components/Header.js
@@ -1,6 +1,8 @@
+import classNames from 'classnames';
 import { get } from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { compose, withStateHandlers } from 'recompose';
 
 /** Components **/
 import Block from 'components/Block';
@@ -8,47 +10,69 @@ import Menu from 'components/Menu';
 
 import styles from './Header.scss';
 
-const MainHeader = ({ menu }) => (
-  <Block className={styles.Root}>
-    <div className={styles.Menu}>
-      <Menu menu={menu} />
-    </div>
+const MainHeader = ({
+  handleHamubrgerClick,
+  menu,
+  menuIsOpened,
+}) => {
+  const className = classNames(styles.Root, {
+    [styles.RootWithMenu]: !!menuIsOpened,
+  });
 
-    <div className={styles.Wrapper}>
-      <div className={styles.Title}>
-        Discover
-        <br />
-        boundless opportunities
-        <br />
-        with Ties.DB
-      </div>
-
-      <div className={styles.Slogan}>
-        A&nbsp;public, distributed and&nbsp;decentralized&nbsp;database
-        <br />
-        with one common thread: trust.
-      </div>
-
-      <form className={styles.Form}>
-        <input
-          className={styles.Input}
-          name="email"
-          placeholder="Email address"
+  return (
+    <Block className={className}>
+      <div className={styles.Menu}>
+        <Menu
+          menu={menu}
+          onHamburgerClick={handleHamubrgerClick}
+          opened={menuIsOpened}
         />
+      </div>
 
-        <button
-          className={styles.Button}
-          type="submit"
-        >
-          Get Early Access
-        </button>
-      </form>
-    </div>
-  </Block>
-)
+      <div className={styles.Wrapper}>
+        <div className={styles.Title}>
+          Discover
+          <br />
+          boundless opportunities
+          <br />
+          with Ties.DB
+        </div>
+
+        <div className={styles.Slogan}>
+          A&nbsp;public, distributed and&nbsp;decentralized&nbsp;database
+          <br />
+          with one common thread: trust.
+        </div>
+
+        <form className={styles.Form}>
+          <input
+            className={styles.Input}
+            name="email"
+            placeholder="Email address"
+          />
+
+          <button
+            className={styles.Button}
+            type="submit"
+          >
+            Get Early Access
+          </button>
+        </form>
+      </div>
+    </Block>
+  );
+}
 
 const mapStateToProps = ({ views }) => ({
   menu: get(views, 'main.menu', []),
 })
 
-export default connect(mapStateToProps)(MainHeader);
+export default compose(
+  connect(mapStateToProps),
+  withStateHandlers(
+    props => ({ menuIsOpened: false }),
+    {
+      handleHamubrgerClick: state => () => ({ menuIsOpened: !state.menuIsOpened }),
+    }
+  )
+)(MainHeader);
