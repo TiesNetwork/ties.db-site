85880dbbffd7029f88b5fe1d97a5df63239fac70
Ivan Vyatkin <vanya095@gmail.com>
2018-04-25 19:16:29 +0300

    Add slider to media block.

--- src/views/Main/components/Media.js
+++ src/views/Main/components/Media.js
@@ -1,5 +1,6 @@
 import { get } from 'lodash';
 import React from 'react';
+import Slider from 'react-slick';
 import { connect } from 'react-redux';
 
 /** Components **/
@@ -7,12 +8,32 @@ import Block from 'components/Block';
 
 import styles from './Media.scss';
 
+const settings = {
+  draggable: false,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        appendDots: dots => <ul className={styles.SliderDots}>{dots}</ul>,
+        arrows: false,
+        dots: true,
+        draggable: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    }
+  ],
+};
+
 const MainMedia = ({ news }) => (
   <Block
     className={styles.Root}
     title="Media"
   >
-    <div className={styles.List}>
+    <Slider {...settings} className={styles.Slider}>
       {news.map(({ date, description, icon, link, title }, index) => (
         <div className={styles.Item} key={index}>
           <a className={styles.ItemTitle} href={link}>
@@ -28,7 +49,7 @@ const MainMedia = ({ news }) => (
           </div>
         </div>
       ))}
-    </div>
+    </Slider>
   </Block>
 )
 
